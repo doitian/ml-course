e52526b2cc1bcac9800af9c4f36ad32a1c117fae
Ian Yang <me@iany.me>
2013-05-09 22:06:52 +0800

    Quiet assignment output

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,7 +8,7 @@ function g = sigmoid(z)
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-g = 1 ./ (1 + e .^ (- z))
+g = 1 ./ (1 + e .^ (- z));
 
 % =============================================================
 
