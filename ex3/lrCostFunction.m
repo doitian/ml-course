26e2f0c6d09078e3a84b49d6fa704122043f041b
Ian Yang <me@iany.me>
2013-05-14 20:37:09 +0800

    Copy ex2 costFunctionReg as ex3 lrCostFunction

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -9,8 +9,8 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -36,17 +36,15 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+J = -1/m * sum(y .* log(h) + (1-y) .* log(1-h)) + lambda / (2*m) * (theta(2:end)' * theta(2:end));
 
-
-
-
-
-
-
+grad = 1/m * (X' * (h - y));
+grad(2:end) += (lambda / m) * theta(2:end);
 
 % =============================================================
 
-grad = grad(:);
+% grad = grad(:);
 
 end
